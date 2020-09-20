ccfe669102d8dc44beaf37a77da03d65a92eb3d8
Libai-cfplhys <55830624+Libai-cfplhys@users.noreply.github.com>
2020-09-20 11:39:38 +0800

    end

--- DKJ/pages/user/user.js
+++ DKJ/pages/user/user.js
@@ -1,12 +1,17 @@
-Component({
-  pageLifetimes: {
-    show() {
-      if (typeof this.getTabBar === 'function' &&
-        this.getTabBar()) {
-        this.getTabBar().setData({
-          selected: 1
-        })
-      }
-    }
+Page({
+  data: {
+    list_icon: [
+      "../../image/app02_22.gif",
+      "../../image/app02_26.gif",
+      "../../image/app02_30.gif",
+      "../../image/app02_33.gif",
+      
+    ],
+    list_text: [
+      "收益报表",
+      "投诉建议",
+      "视频素材",
+      "系统设置"
+    ],
   }
 })
