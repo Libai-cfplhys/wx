ea2e827ae52cffed11baf3190a52013047769790
unknown <709178881@qq.com>
2020-11-18 17:07:46 +0800

    创建项目

---
+++ wow/components/banner/banner.vue
@@ -0,0 +1,33 @@
+<template>
+	<view>
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+			<swiper-item>
+				<view class="bg-red"></view>
+			</swiper-item>
+			<swiper-item>
+				<view class="bg-blue"></view>
+			</swiper-item>
+		</swiper>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+	.bg-red{
+		width: 100%;
+		height: 100%;
+	}
+	.bg-blue{
+		width: 100%;
+		height: 100%;
+	}
+</style>
---
+++ wow/pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="content">
+		<banner></banner>
+	</view>
+</template>
+
+<script>
+	import banner from "@/components/banner/banner.vue"
+	export default {
+		components:{
+			banner
+		},
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content{
+		width: 750rpx;
+	}
+</style>
